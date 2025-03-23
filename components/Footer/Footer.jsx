d778611cb77adf5c67eb4e4d2695ea3be3d70ded
Jan <kupper.thorge@gmail.com>
2025-03-23 15:27:37 +0100

    ui adjustments

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="bg-muted mt-auto border-t py-8">
       <div className="mx-auto flex max-w-5xl">
-        <div className="grid w-full grid-cols-1 justify-items-start sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid-cols grid w-full grid-cols-2 sm:grid-cols-3">
           <Button variant={"link"}>Support</Button>
           <Button variant={"link"}>Security</Button>
           <Button variant={"link"}>Financial Education</Button>
@@ -14,16 +14,6 @@ export default function Footer() {
           <Button variant={"link"}>Online tracking</Button>
           <Button variant={"link"}>Advertisement</Button>
           <Button variant={"link"}>Privacy</Button>
-        </div>
-        <div className="px-4">
-          <p className="text-primary text-sm font-medium">Address:</p>
-          <p className="text-primary text-sm font-light text-nowrap">
-            Big Stacks Bank
-            <br />
-            Arenavägen 61
-            <br />
-            121 77 Johanneshov
-          </p>
         </div>
       </div>
     </div>
